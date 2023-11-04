102727a4b51372db89cf81207ede344b22cbeac0
desynkd <ravindupeeris180@gmail.com>
2023-11-04 13:52:07 +0530

    create patient dashboard

---
+++ patient_dashboard.php
@@ -0,0 +1,38 @@
+<?php 
+require_once 'includes/config_session.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Pharmalink - Patient</title>
+</head>
+<body>
+<div class='bold-line'></div>
+    <div class='container'>
+    <div class='window'>
+        <div class='overlay'></div>
+        <div class='content'>
+            <?php
+                echo "<div class='welcome'>Hello, " . $_SESSION["user_username"] . "</div>";
+                echo "<div class='subtitle'>You are logged in as " . $_SESSION["user_type"] . "</div>";
+            ?>
+
+            <form action="patient_profile.php" method="post">
+                <div style="padding: 30px 10px 5px;" ><button class='ghost-round full-width'>View Profile</button></div>
+            </form>
+            <form action="patient_vitalsign.php" method="post">
+                <div style="padding: 5px 10px 5px;" ><button class='ghost-round full-width'>View Vital Sign Records</button></div>
+            </form>
+            <form action="includes/logout.inc.php" method="post">
+                <div style="padding: 5px 10px 10px;" ><button class='ghost-round full-width'>Logout</button></div>
+            </form>
+            
+        </div>
+    </div>
+    </div>
+</body>
+</html>
